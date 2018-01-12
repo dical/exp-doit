3f8c348f3e4e634ae0bca3502a09153440ac3abb
cutshadows <Cutshadows@users.noreply.github.com>
2018-01-12 15:36:13 -0300

    doitexp problem
    no puedo mostar el perfil del usuario creado

--- passport.js
+++ passport.js
@@ -10,7 +10,6 @@ var mongoose = require('mongoose');
 
 module.exports = function(passport) {
     //configurar nuestra autenticacion
-    //1. definir la estrategiaco
     
  passport.use(new FacebookStrategy({
          clientID: config.facebook.id,
@@ -31,21 +30,30 @@ module.exports = function(passport) {
                 var fechaDato = string.split('/');
                 return fechaDato[2] + '-' + fechaDato[1] + '-' + fechaDato[0];
             };
-            
+            var number=profile._json.id;
+            function random(number){
+                var digitos= Math.round(Math.random()*number);
+                return digitos;
+                
+            };
             
         User.findOne({social:{facebook:{uid:profile.id}},social:{facebook:{provider:profile.provider}}}, function(err,user){
-                if(err) throw(err);
+                if(err){throw(err);
+                }
+
                 if(!err && user!= null){ 
                     return cb(null, user)
                 }else{
                 
                 var user= new User();
-                    user.username=profile.displayName;
+                    user.username=profile.name.familyName+''+random(number.substring(0, 4));
                     user.names=profile.name.givenName + ' ' + profile.name.familyName;
-                    user.password='doitexp@12345';
+                    user.password='doitexp12345';
                     user.surnames=profile.name.familyName;
                     user.phrase='Edita tu frase en la rueda ubicada en la parte superior derecha';
+                   
                     user.mail=profile.emails[0].value;
+                   
                     user.social={
                         facebook:{
                                     accessToken: accessToken,
@@ -53,14 +61,6 @@ module.exports = function(passport) {
                                     uid: profile.id 
                                 }
                     },
-                    user.direction={
-                        city:{
-                            id:profile._json.location.id,
-                            name:profile._json.location.name
-                        },
-                        street:'' ,
-                        location: '0'
-                    };
                     user.born=convertDateFormat(fecha);
                     user.image=profile.photos[0].value;
 
@@ -89,12 +89,9 @@ module.exports = function(passport) {
     });
         // definir como vamos a retomar el usuario de la session
     passport.deserializeUser(function(id, done){
-        User.findById(id._id, function(err, user) {
-            done(err, user);
-           console.log("hola usuario", user);
-           /* if(user.password == 'doitexp@12345'){
-                console.log("debe cambiar el password");
-            }*/
+        User.findById(id._id, function(error, user) {
+            done(error, user);
+
        });
         //aca se busca en la base de dats y se muestra el usuario
         done(null, id);
